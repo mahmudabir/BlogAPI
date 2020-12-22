4f82de3f17d0a59388d1e43b43327b614e40f012
Abir Mahmud <amabirmahmud@gmail.com>
2020-12-22 16:45:47 +0600

    Update 202012182054582_UserDataInput.cs

--- ATP2-Final-Assignment/Migrations/202012182054582_UserDataInput.cs
+++ ATP2-Final-Assignment/Migrations/202012182054582_UserDataInput.cs
@@ -2,14 +2,14 @@ namespace ATP2_Final_Assignment.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class UserDataInput : DbMigration
     {
         public override void Up()
         {
-            Sql("INSERT INTO Users (Username, Password) VALUES ('1','1'), ('2', '2'),('3', '3'),('4', '4'),('abir', 'abir'),('leon', 'leon')");
+            Sql("INSERT INTO Users (Username, Password) VALUES ('abir', 'abir'),('leon', 'leon'),('hasib','hasib'), ('tanvir', 'tanvir'),('imran', 'imran'),('milon', 'milon')");
         }
-        
+
         public override void Down()
         {
         }
